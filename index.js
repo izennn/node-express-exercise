0f491aa0d91387ae774b42d8fed1f3df26ba9ab9
izen.oku <izenhuang@hotmail.com>
2020-09-20 13:58:18 -0700

    feat: rinstantiated outes folder includes dishRouter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,58 +12,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	next(); // will continue to look for functions that match '/dishes/ endpoint
-});
-
-app.get('/dishes', (req, res, next) => {
-	// now res is modified
-	res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req, res, next) => {
-	// post requests carry some info in body
-	console.log(res.body);
-	res.end('Will add info to dish: ' + req.body.name
-	+ " with details: " + req.body.description); // expect a "name" field in JSON 
-});
-
-app.put('/dishes', (req, res, next) => {
-	res.statusCode = 403;
-	res.end('PUT operation not supported on /dishes');
-})
-
-app.delete('/dishes', (req, res, next) => {
-	// later we will restrict this dangerous operation to authorized users
-	res.statusCode = 403;
-	res.end('DELETE operation not supported on /dishes');
-})
-
-
-app.get('/dishes/:dishId', (req, res, next) => {
-	// now res is modified
-	res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-	// post requests carry some info in body
-	res.statusCode = 403;
-	res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-	res.write('Updating the dish: ' + req.params.dishId + '\n');
-	res.end(`Will udpate the dish: ${req.body.name} with details: ${req.body.description}`);
-})
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-	// later we will restrict this dangerous operation to authorized users
-	res.statusCode = 403;
-	res.end(`Deleting dish ${req.params.dishId}`);
-})
-
+app.use('/dishes', dishRouter); // mount dish router to overall app
 app.use(express.static(__dirname+'/public'));
 
 // Here we are creating a "middleware function" which has access to req, res, and next middleware
---
+++ routes/dishRouter.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// API restpoint handling for /dishes
+const dishRouter = express.Router();
+
+dishRouter.use(bodyParser.json())
+
+// route takes endpoint as parameter
+dishRouter.route('/')
+.all((req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	next(); // will continue to look for functions that match '/dishes/ endpoint
+})
+.get((req, res, next) => {
+	// now res is modified
+	res.end('Will send all the dishes to you!');
+})
+.post((req, res, next) => {
+	// post requests carry some info in body
+	console.log(res.body);
+	res.end('Will add info to dish: ' + req.body.name
+	+ " with details: " + req.body.description); // expect a "name" field in JSON 
+})
+.put((req, res, next) => {
+	res.statusCode = 403;
+	res.end('PUT operation not supported on /dishes');
+})
+.delete((req, res, next) => {
+	// later we will restrict this dangerous operation to authorized users
+	res.statusCode = 403;
+	res.end('DELETE operation not supported on /dishes');
+})
+
+module.exports = dishRouter;
+
+
+// app.get('/dishes/:dishId', (req, res, next) => {
+// 	// now res is modified
+// 	res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+// });
+
+// app.post('/dishes/:dishId', (req, res, next) => {
+// 	// post requests carry some info in body
+// 	res.statusCode = 403;
+// 	res.end('POST operation not supported on /dishes/' + req.params.dishId);
+// });
+
+// app.put('/dishes/:dishId', (req, res, next) => {
+// 	res.write('Updating the dish: ' + req.params.dishId + '\n');
+// 	res.end(`Will udpate the dish: ${req.body.name} with details: ${req.body.description}`);
+// })
+
+// app.delete('/dishes/:dishId', (req, res, next) => {
+// 	// later we will restrict this dangerous operation to authorized users
+// 	res.statusCode = 403;
+// 	res.end(`Deleting dish ${req.params.dishId}`);
+// })
