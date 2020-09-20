aa16719af22b1d83c32a3aa8c9c537ce33df8e4e
izen.oku <izenhuang@hotmail.com>
2020-09-20 12:27:43 -0700

    feat: express.static to access public files

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
-// 
+// app object conventionally denotes the Express app
 const app = express();
 
+app.use(express.static(__dirname+'/public'));
+
+// Here we are creating a "middleware function" which has access to req, res, and next middleware
 app.use((req, res, next) => {
 	// next is used when you need to invoke additional middleware
-	console.log(req.headers);
 
 	res.statusOcde = 200;
 	res.setHeader('Content-Type', 'text/html');
 	res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-})
+});
+
+// app.use() binds app level middlware to instance of app object
+// app.use('path', () => {}) handles any type of HTTP request on the specified path
 
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
